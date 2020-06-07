9c8518c2f4393942179ef86d09651933b70a9561
zhaoxianqiang <24930662@qq.com>
2020-06-07 11:24:34 +0800

    add nodejs server

---
+++ 09-nodejs/webServer.js
@@ -0,0 +1,40 @@
+const http = require('http')
+
+const hostname = '127.0.0.1'
+const port = 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'application/json')
+  var data =  [{
+        name: "《算法导论》",
+        beginDate: "2006-9",
+        price: 85.00,
+        count: 1
+      },
+      {
+        name: "《UNIX编程艺术》",
+        beginDate: "2006-2",
+        price: 59.00,
+        count: 1
+      },
+      {
+        name: "《编程大全》",
+        beginDate: "2008-10",
+        price: 39.00,
+        count: 1
+      },
+      {
+        name: "《代码大全》",
+        beginDate: "2006-3",
+        price: 128.00,
+        count: 1
+      },
+    ]
+  // retrun data json format
+  res.end(JSON.stringify(data))
+})
+
+server.listen(port, hostname, () => {
+  console.log(`服务器运行在 http://${hostname}:${port}/`)
+})
